72ad3b1cd257505817155647e2c2644aa3b27400
Delaney Baker <114117065+delaneyb22@users.noreply.github.com>
2023-10-28 21:55:56 -0500

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace ScriptureJournalApp
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddDbContext<ScriptureJournalDbContext>(options =>
+            options.UseSqlServer(Configuration.GetConnectionString("ScriptureJournalDbContext")));
+
+        services.AddControllersWithViews();
+    }
+}
+
